b407dec8a29e583d5316a767fbe7224f177ab621
Manoel Duran <manoel.duran@hotmail.com>
2022-03-08 14:50:01 -0300

    finalizado

--- src/pages/Movie/styles.ts
+++ src/pages/Movie/styles.ts
@@ -1,18 +1,22 @@
 import styled from "styled-components";
 
+interface BackgroundImageProps {
+  imgUrl: string;
+}
+
 export const Container = styled.div`
-width: 100vw;
-height: 100vh;
 background-color: ${({ theme }) => theme.black} ;
 `;
 
 export const Content = styled.div`
-width: 100% ;
-height: 100% ;
 display: flex;
 align-items: center;
-@media screen and (max-width: 450px){
-  margin-top: 0rem;
+justify-content: center;
+width: 100%;
+
+@media screen and (max-width: 950px){
+  height: 100%;
+  margin-top: 1.5rem;
 display: flex ;
 flex-direction: column;
 align-items: center;
@@ -27,6 +31,27 @@ width: 100% ;
  padding-left: 10rem ;
  text-align: left ;
  cursor: pointer ;
+ @media screen and (max-width: 1900px){
+  margin-bottom: 5rem ;
+};
+@media screen and (max-width: 1700px){
+  margin-bottom: 4rem ;
+};
+@media screen and (max-width: 1450px){
+  margin-bottom: 3rem ;
+};
+@media screen and (max-width: 1000px){
+  padding-left: 4rem ;
+};
+@media screen and (max-width: 850px){
+  padding-left: 2rem ;
+};
+@media screen and (max-width: 780px){
+  padding-left: 1.5rem ;
+};
+@media screen and (max-width: 600px){
+padding-top: 1rem ;
+};
 `;
 
 export const Info = styled.div`
@@ -34,12 +59,69 @@ display: flex ;
 flex-direction: column ;
 align-items: center ;
 justify-content: space-between ;
+@media screen and (max-width: 700px){
+  height: 100vh ;
+  display: flex ;
+flex-direction: column ;
+align-items: center ;
+justify-content: center ;
+};
 `;
 
 export const Title = styled.h1`
 font-size: 2.5rem;
  text-align: center;
  color: ${({ theme }) => theme.white_details};
+ @media screen and (max-width: 1900px){
+  text-align:  justify;
+line-height: 7rem ;
+font-size: 4.5rem;
+};
+@media screen and (max-width: 1700px){
+  text-align:  justify;
+line-height: 6rem ;
+font-size: 4rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+line-height: 5rem ;
+font-size: 4rem;
+};
+@media screen and (max-width: 1080px){
+  text-align:  justify;
+line-height: 4.5rem ;
+font-size: 3rem;
+};
+@media screen and (max-width: 1000px){
+line-height: 4rem ;
+text-align:  justify;
+font-size: 2.5rem;
+};
+@media screen and (max-width: 850px){
+line-height: 3rem ;
+text-align:  justify;
+font-size: 2.5rem;
+};
+@media screen and (max-width: 780px){
+line-height: 3rem ;
+text-align:  justify;
+font-size: 2rem;
+};
+@media screen and (max-width: 715px){
+line-height: 3rem ;
+text-align:  justify;
+font-size: 1.8rem;
+};
+@media screen and (max-width: 400px){
+line-height: 2rem ;
+text-align:  justify;
+font-size: 2.5rem;
+};
+@media screen and (max-width: 308px){
+line-height: 1rem ;
+text-align:  justify;
+font-size: 1.5rem;
+};
  `;
 
 export const NameContainer = styled.div`
@@ -59,8 +141,8 @@ justify-content: center ;
 
 
 export const Details = styled.div`
-width: 60%;
 padding: 30px;
+
 `;
 
 
@@ -69,26 +151,66 @@ text-align:  justify;
 line-height: 1.5rem ;
 font-size: 1.2rem;
 color: ${({ theme }) => theme.white_details};
-@media screen and (max-width: 800px){
-  text-align: justify;
-font-size: 0.6rem;
+@media screen and (max-width: 1900px){
+  text-align:  justify;
+line-height: 3.5rem ;
+font-size: 2rem;
 };
-@media screen and (max-width: 450px){
-  text-align: start;
-font-size: 0.5rem;
-margin-left: 15px;
+@media screen and (max-width: 1700px){
+  text-align:  justify;
+line-height: 3rem ;
+font-size: 2rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+line-height: 2rem ;
+font-size: 1.6rem;
+};
+@media screen and (max-width: 1200px){
+  text-align:  justify;
+line-height: 2rem ;
+font-size: 1.4rem;
+};
+@media screen and (max-width: 1100px){
+  text-align:  justify;
+line-height: 2rem ;
+font-size: 1.3rem;
+};
+@media screen and (max-width: 1000px){
+  text-align:  justify;
+line-height: 2rem ;
+font-size: 1.2rem;
+};
+@media screen and (max-width: 850px){
+  text-align:  justify;
+line-height: 1.7rem ;
+font-size: 1.2rem;
+};
+@media screen and (max-width: 780px){
+  text-align:  justify;
+line-height: 1.7rem ;
+font-size: 1.1rem;
+};
+@media screen and (max-width: 700px){
+  text-align:  justify;
+line-height: 1.5rem ;
+font-size: 1.1rem;
 };
 `;
 
 export const Poster = styled.img`
 height: 100vh;
+width: 100% ;
 border-radius: 100% 30% 100% 30% ;
-  @media screen and (max-width: 450px){
-    border: none;
+
+@media screen and (max-width: 950px){
+  border-radius: 0%;
+  width: 100% ;
+};
+@media screen and (max-width: 700px){
+  border: none;
     margin-left: 0px;
     border-radius: 0%;
-    height: 400px;
-    width: 100%;
 };
 `;
 
@@ -98,20 +220,109 @@ width: 100% ;
  font-size: 1.2rem;
  padding-left: 10rem ;
  text-align: left ;
+ @media screen and (max-width: 1900px){
+  text-align:  justify;
+line-height: 2rem ;
+font-size: 1.5rem;
+};
+@media screen and (max-width: 1700px){
+  text-align:  justify;
+line-height: 1.5rem ;
+font-size: 1.5rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+line-height: 1.5rem ;
+font-size: 1rem;
+};
+@media screen and (max-width: 1200px){
+  text-align:  justify;
+line-height: 1.5rem ;
+font-size: 1rem;
+};
+@media screen and (max-width: 1000px){
+  padding-left: 4rem ;
+};
+@media screen and (max-width: 850px){
+  padding-left: 2rem ;
+};
+@media screen and (max-width: 780px){
+  padding-left: 1.5rem ;
+};
+@media screen and (max-width: 500px){
+padding-bottom: 1.5rem ;
+};
 `;
 export const ReleaseDate = styled.span`
 font-size: 1.2rem ;
  color: ${({ theme }) => theme.white_details};
  text-align: left ;
+ @media screen and (max-width: 1900px){
+line-height: 2rem ;
+font-size: 1.7rem;
+};
+@media screen and (max-width: 1700px){
+  text-align:  justify;
+  line-height: 1.8rem ;
+font-size: 1.7rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.6rem;
+};
+@media screen and (max-width: 850px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.4rem;
+};
+@media screen and (max-width: 780px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.3rem;
+};
+@media screen and (max-width: 400px){
+  text-align:  justify;
+  line-height: 1.5rem ;
+font-size: 1.1rem;
+};
 `;
 export const Language = styled.span`
 font-size: 1.2rem ;
  text-align: center;
  padding-left: 15px ;
+ @media screen and (max-width: 1900px){
+line-height: 2rem ;
+font-size: 1.7rem;
+};
+@media screen and (max-width: 1700px){
+  line-height: 1.8rem ;
+font-size: 1.7rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.6rem;
+};
+@media screen and (max-width: 850px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.4rem;
+};
+@media screen and (max-width: 780px){
+  text-align:  justify;
+  line-height: 1.7rem ;
+font-size: 1.3rem;
+};
+@media screen and (max-width: 400px){
+  text-align:  justify;
+  line-height: 1.5rem ;
+font-size: 1.1rem;
+};
 `;
 
 export const Trailer = styled.button`
-width: 30%;
+width: 100%;
 height: 50px ;
 margin-top: 15px ;
 padding: 30px;
@@ -122,9 +333,42 @@ justify-content: space-evenly;
 border: none ;
 background-color: ${({ theme }) => theme.black} ;
 border: 1px solid ${({ theme }) => theme.border};
+@media screen and (max-width: 1900px){
+  margin-top: 2rem ;
+  width: 50%;
+height: 80px ;
+};
+@media screen and (max-width: 1000px){
+  width: 40%;
+padding: 15px;
+};
+
+@media screen and (max-width: 440px){
+  width: 50%;
+padding: 15px;
+};
 `;
 
 export const ButtonText = styled.span`
 font-size: 1.2rem ;
  color: ${({ theme }) => theme.white_details};
+ @media screen and (max-width: 1900px){
+line-height: 2rem ;
+font-size: 1.5rem;
+};
+@media screen and (max-width: 1450px){
+  text-align:  justify;
+  line-height: 1.5rem ;
+font-size: 1.3rem;
+};
+@media screen and (max-width: 1200px){
+  text-align:  justify;
+  line-height: 1.4rem ;
+font-size: 1.2rem;
+};
+@media screen and (max-width: 780px){
+  text-align:  justify;
+  line-height: 1.3rem ;
+font-size: 1.1rem;
+};
 `;
